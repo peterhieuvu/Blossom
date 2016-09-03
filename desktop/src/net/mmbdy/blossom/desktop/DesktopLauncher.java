d04f4afcc7a081bda250ec9792e72f5149c32d7b
Meeesh <peter.vu8@gmail.com>
2016-09-03 13:44:58 -0700

    LibGDX Initialization

---
+++ desktop/src/net/mmbdy/blossom/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.mmbdy.blossom.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.mmbdy.blossom.Blossom;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Blossom(), config);
+	}
+}
---
+++ html/src/net/mmbdy/blossom/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package net.mmbdy.blossom.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import net.mmbdy.blossom.Blossom;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Blossom();
+        }
+}
